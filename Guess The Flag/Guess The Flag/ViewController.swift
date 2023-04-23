f5bf575ef5c54cbc4fe68472b7007acc0726d061
Caglar <asdwas94@hotmail.com>
2023-04-23 17:25:45 +0300

    Initiated Project

---
+++ Guess The Flag/Guess The Flag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guess The Flag
+//
+//  Created by Ali ÇAĞLAR on 23.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
