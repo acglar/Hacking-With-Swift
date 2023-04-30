166be72ad710b9a2ef50454ec31eade820fc7218
Caglar <asdwas94@hotmail.com>
2023-04-30 17:05:32 +0300

    Initiates Project

---
+++ Easy Browser/Easy Browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Easy Browser
+//
+//  Created by Ali ÇAĞLAR on 30.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
