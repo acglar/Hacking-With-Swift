2e450f1708396ae56b74ea31fbf28cf810c18276
Caglar <asdwas94@hotmail.com>
2023-05-02 08:49:51 +0300

    Initiated Project

---
+++ Word Scramble/Word Scramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Word Scramble
+//
+//  Created by Ali ÇAĞLAR on 2.05.2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
