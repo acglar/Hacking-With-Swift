66e8b30f0b33f5dba7e95afb40d8c4cb231aff7e
Caglar <asdwas94@hotmail.com>
2023-05-02 23:28:43 +0300

    Added Submitting answer and checking if it's valid logic

--- Word Scramble/Word Scramble/ViewController.swift
+++ Word Scramble/Word Scramble/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UITableViewController {
         }
         
         startGame()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
     }
     
     internal override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -32,6 +34,60 @@ class ViewController: UITableViewController {
         content.text = usedWords[indexPath.row]
         cell.contentConfiguration = content
         return cell
+    }
+    
+    @objc private func promptForAnswer() {
+        let alertController = UIAlertController(title: "Answer", message: nil, preferredStyle: .alert)
+        alertController.addTextField()
+        
+        let submitAnswer = UIAlertAction(title: "Submit", style: .default) { [weak self, weak alertController] _ in
+            guard let answer = alertController?.textFields?[0].text else { return }
+            self?.submit(answer: answer)
+        }
+        
+        alertController.addAction(submitAnswer)
+        present(alertController, animated: true)
+    }
+    
+    private func submit(answer: String) {
+        let lowerCasedAnswer = answer.lowercased()
+        
+        if isValid(word: lowerCasedAnswer) {
+            usedWords.insert(lowerCasedAnswer, at: 0)
+            
+            let indexPath = IndexPath(row: 0, section: 0)
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    private func isValid(word: String) -> Bool {
+        return isPossible(word: word) && isOriginal(word: word) && isReal(word: word)
+    }
+    
+    private func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    private func isPossible(word: String) -> Bool {
+        guard var anagramWord = title?.lowercased() else { return false }
+        
+        for letter in word {
+            if let index = anagramWord.firstIndex(of: letter) {
+                anagramWord.remove(at: index)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    private func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
     }
     
     private func startGame() {
