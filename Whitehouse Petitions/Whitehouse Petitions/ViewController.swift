12919c92b84f4eb96e08a9fabd0d9321907e0c55
Caglar <asdwas94@hotmail.com>
2023-05-07 20:43:41 +0300

    Created Basic UI

--- Whitehouse Petitions/Whitehouse Petitions/ViewController.swift
+++ Whitehouse Petitions/Whitehouse Petitions/ViewController.swift
@@ -7,11 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    let navigationControllerID: String = "NavController"
+    let tableViewCellIdentifier: String = "Cell"
+    var petitions: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: tableViewCellIdentifier, for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "Title Spot"
+        content.secondaryText = "Subtitle Spot"
+        cell.contentConfiguration = content
+        return cell
     }
 
 
