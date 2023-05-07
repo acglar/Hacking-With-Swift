df5e24795475828d6897bde1d3e0a8cfc16194c4
Caglar <asdwas94@hotmail.com>
2023-05-07 17:44:30 +0300

    Initiated Project

---
+++ Whitehouse Petitions/Whitehouse Petitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Whitehouse Petitions
+//
+//  Created by Ali ÇAĞLAR on 7.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
