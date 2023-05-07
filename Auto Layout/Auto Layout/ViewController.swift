c6ded7d11aecfe2b50a67183956702a427e2df5b
Caglar <asdwas94@hotmail.com>
2023-05-07 15:32:58 +0300

    Updated Constraints with anchors

--- Auto Layout/Auto Layout/ViewController.swift
+++ Auto Layout/Auto Layout/ViewController.swift
@@ -19,8 +19,9 @@ class ViewController: UIViewController {
         let sentence = "These are some awesome labels"
         let words = sentence.uppercased().components(separatedBy: " ")
         let colors = [UIColor.red, UIColor.green, UIColor.blue, UIColor.yellow, UIColor.magenta]
-        var viewsDictionary: [String:UILabel] = [:]
+        //var viewsDictionary: [String:UILabel] = [:]
         
+        var previousLabel: UILabel?
         for i in 0..<5 {
             let label = UILabel()
             label.translatesAutoresizingMaskIntoConstraints = false
@@ -29,16 +30,27 @@ class ViewController: UIViewController {
             label.sizeToFit()
             
             view.addSubview(label)
-            viewsDictionary["label\(i)"] = label
+            //viewsDictionary["label\(i)"] = label
+            
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 80).isActive = true
+            
+            if let previous = previousLabel {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+            }
+            
+            previousLabel = label
         }
         
-        for label in viewsDictionary.keys {
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
-        }
-        
-        let metrics = ["labelHeight": 80]
-        
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label0(labelHeight@999)]-[label1(label0)]-[label2(label0)]-[label3(label0)]-[label4(label0)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+//        for label in viewsDictionary.keys {
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
+//
+//        let metrics = ["labelHeight": 80]
+//
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label0(labelHeight@999)]-[label1(label0)]-[label2(label0)]-[label3(label0)]-[label4(label0)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
     }
 
 }
