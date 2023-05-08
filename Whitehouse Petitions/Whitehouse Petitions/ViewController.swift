5659e612eccf74a8e0354a056835f9037a4c8237
Caglar <asdwas94@hotmail.com>
2023-05-08 23:17:17 +0300

    Updated Json data for most viewed

--- Whitehouse Petitions/Whitehouse Petitions/ViewController.swift
+++ Whitehouse Petitions/Whitehouse Petitions/ViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 class ViewController: UITableViewController {
     let tableViewCellIdentifier: String = "Cell"
     var petitions: [Petition] = []
-    let urlString: String = "https://www.hackingwithswift.com/samples/petitions-1.json"
+    var urlString: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let url = URL(string: urlString) {
-            if let data = try? Data(contentsOf: url) {
-                parse(json: data)
-            }
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
     }
     
