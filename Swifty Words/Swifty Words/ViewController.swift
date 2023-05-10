c75ad02b093597ce800954f94d08d4b3b5ddd217
Caglar <asdwas94@hotmail.com>
2023-05-10 22:40:24 +0300

    Initiated Project

---
+++ Swifty Words/Swifty Words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swifty Words
+//
+//  Created by Ali ÇAĞLAR on 10.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
