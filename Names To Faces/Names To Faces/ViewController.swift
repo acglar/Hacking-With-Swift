7f97303c334cba861c9cc447e5ad43f483df17ae
Caglar <asdwas94@hotmail.com>
2023-05-14 23:26:58 +0300

    Initiated Project

---
+++ Names To Faces/Names To Faces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Names To Faces
+//
+//  Created by Ali ÇAĞLAR on 14.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
