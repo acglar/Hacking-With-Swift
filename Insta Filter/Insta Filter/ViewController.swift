54e796938b58e06a77ae26bb959e1c1c7313f97f
Caglar <asdwas94@hotmail.com>
2023-05-19 14:26:05 +0300

    Linked UI items to view controller

--- Insta Filter/Insta Filter/ViewController.swift
+++ Insta Filter/Insta Filter/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensitySlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onChangeFilterButtonClick(_ sender: Any) {
+    }
+    
+    @IBAction func onSaveButtonClick(_ sender: Any) {
+    }
+    
+    @IBAction func onIntensityValueChanged(_ sender: Any) {
+    }
 }
 
