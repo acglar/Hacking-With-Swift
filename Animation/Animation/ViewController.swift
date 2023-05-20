47f61564c4f69b32c3455dae4562c3be6f74c449
Caglar <asdwas94@hotmail.com>
2023-05-20 16:44:35 +0300

    Initiated Project

---
+++ Animation/Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Ali ÇAĞLAR on 20.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
