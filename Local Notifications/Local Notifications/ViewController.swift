172a6e7e446ea1ec435b69fa24301a8cd111a07a
Caglar <asdwas94@hotmail.com>
2023-05-21 15:57:43 +0300

    Initiated Project

---
+++ Local Notifications/Local Notifications/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Local Notifications
+//
+//  Created by Ali ÇAĞLAR on 21.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
